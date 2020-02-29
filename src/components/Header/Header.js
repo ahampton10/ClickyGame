c9c993160aa9bf86220e8d3bd68b8cda98a8901e
Alex H <hampton.alex10@gmail.com>
2020-02-29 08:52:58 -0700

    Working

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="title">
+      <h1>Clicky Game</h1>
+      <p>Click all images below without clicking the same one twice!</p>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/images.js
@@ -0,0 +1,58 @@
+const imageArray = [
+  {
+    name: "mountain1",
+    url:
+      "https://images.pexels.com/photos/417173/pexels-photo-417173.jpeg?cs=srgb&dl=altitude-clouds-cold-daylight-417173.jpg&fm=jpg",
+    clicked: false
+  },
+  {
+    name: "mountain2",
+    url:
+      "https://images.pexels.com/photos/691668/pexels-photo-691668.jpeg?cs=srgb&dl=landscape-photography-of-mountains-covered-in-snow-691668.jpg&fm=jpg",
+    clicked: false
+  },
+  {
+    name: "mountain3",
+    url:
+      "https://images.pexels.com/photos/291732/pexels-photo-291732.jpeg?cs=srgb&dl=adventure-cold-daylight-fog-291732.jpg&fm=jpg",
+    clicked: false
+  },
+  {
+    name: "mountain4",
+    url:
+      "https://images.pexels.com/photos/1054218/pexels-photo-1054218.jpeg?cs=srgb&dl=snow-top-mountain-under-clear-sky-1054218.jpg&fm=jpg",
+    clicked: false
+  },
+  {
+    name: "mountain5",
+    url:
+      "https://images.pexels.com/photos/361104/pexels-photo-361104.jpeg?cs=srgb&dl=evergreen-glacier-hike-hill-361104.jpg&fm=jpg",
+    clicked: false
+  },
+  {
+    name: "mountain6",
+    url:
+      "https://images.pexels.com/photos/540518/pexels-photo-540518.jpeg?cs=srgb&dl=clouds-cold-frost-frosty-540518.jpg&fm=jpg",
+    clicked: false
+  },
+  {
+    name: "mountain7",
+    url:
+      "https://images.pexels.com/photos/714258/pexels-photo-714258.jpeg?cs=srgb&dl=mountain-ranges-covered-in-snow-714258.jpg&fm=jpg",
+    clicked: false
+  },
+  {
+    name: "mountain8",
+    url:
+      "https://images.pexels.com/photos/673020/pexels-photo-673020.jpeg?cs=srgb&dl=grand-canyon-673020.jpg&fm=jpg",
+    clicked: false
+  },
+  {
+    name: "mountain9",
+    url:
+      "https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?cs=srgb&dl=adventure-cold-conifers-evening-572897.jpg&fm=jpg",
+    clicked: false
+  }
+];
+
+export default imageArray;
